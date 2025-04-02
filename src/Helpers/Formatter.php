f056e2268b4ab2aff3c2c395dbdcd0dbbdb8d5e1
rifat <rifat.simoom@softrobotics.com.bd>
2025-04-03 01:52:07 +0600

    fix! formatQuery using preg_replace

--- src/Helpers/Formatter.php
+++ src/Helpers/Formatter.php
@@ -6,21 +6,10 @@ class Formatter
 {
     public static function formatQuery(array $bindings, string $query): string
     {
-        // Find all positions of "?" in the query
-        $positions = [];
-        $offset = 0;
-        while (($pos = strpos($query, '?', $offset)) !== false) {
-            $positions[] = $pos;
-            $offset = $pos + 1;
+        foreach ($bindings as $binding) {
+            $escapedBinding = preg_quote($binding, '/');
+            $query = preg_replace('/\?/', "'$escapedBinding'", $query, 1);
         }
-
-        for ($i = count($positions) - 1; $i >= 0; $i--) {
-            if (isset($bindings[$i])) {
-                $binding = $bindings[$i];
-                $query = substr_replace($query, "'$binding'", $positions[$i], 1);
-            }
-        }
-
         return $query;
     }
 }
