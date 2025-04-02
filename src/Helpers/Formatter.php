a4acad4b7513568b6c4bd92781972b6e9f611f74
rifat <rifat.simoom@softrobotics.com.bd>
2025-04-03 01:09:41 +0600

    fix! formatQuery to correctly handle bindings with special characters

--- src/Helpers/Formatter.php
+++ src/Helpers/Formatter.php
@@ -6,9 +6,21 @@ class Formatter
 {
     public static function formatQuery(array $bindings, string $query): string
     {
-        foreach ($bindings as $binding) {
-            $query = preg_replace('/\?/', "'$binding'", $query, 1);
+        // Find all positions of "?" in the query
+        $positions = [];
+        $offset = 0;
+        while (($pos = strpos($query, '?', $offset)) !== false) {
+            $positions[] = $pos;
+            $offset = $pos + 1;
         }
+
+        for ($i = count($positions) - 1; $i >= 0; $i--) {
+            if (isset($bindings[$i])) {
+                $binding = $bindings[$i];
+                $query = substr_replace($query, "'$binding'", $positions[$i], 1);
+            }
+        }
+
         return $query;
     }
 }
